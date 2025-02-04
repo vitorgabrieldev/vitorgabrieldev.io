4cedcf7d0d44dd3066e9b13bbc47a09500ee80cb
Vitor Gabriel de Oliveira <vitorgabrieldeoliveiragit@gmail.com>
2025-02-04 14:14:50 -0300

    Feat ✨ | Implementação de pretty

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
-    let autoScroll = false;
-    let lastScrollTop = 0;
 
-    $("html, body").animate({ scrollTop: 0 }, 1000); // Garante que a página começa no topo
+    $(".preety-print").hide();
+
+    let autoScroll = false;
+    $("html, body").animate({ scrollTop: 0 }, 1000);
 
     function typeEffect(element, text, callback) {
         let currentLength = 0;
@@ -19,38 +20,103 @@ $(document).ready(function() {
         }, typingSpeed);
     }
 
-    function smoothScroll(direction) {
-        let currentScrollTop = $(window).scrollTop();
-        let targetScrollTop = direction === 'up' ? Math.max(currentScrollTop - 300, 0) : currentScrollTop + 300;
-        $("html, body").animate({ scrollTop: targetScrollTop }, 1000);
-    }
-
     typeEffect($(".header-title .up"), "HI!", function() {
         typeEffect($(".header-title .down"), "I am Vitor Gabriel", function() {
             typeEffect($(".header-subtitle"), "FULL STACK DEVELOPER | DEVOPS ENTHUSIAST", function() {
                 $("body").css("overflow-y", "auto");
 
-                // Habilitar rolagem automática do mouse
-                $(window).on('mousemove', function(e) {
-                    let windowHeight = $(window).height();
-                    let mouseY = e.clientY;
-
-                    // Se o mouse estiver mais perto do topo, rolar para cima
-                    if (mouseY < 50) {
-                        if (lastScrollTop !== 0) {
-                            smoothScroll('up');
-                            lastScrollTop = 0;
-                        }
-                    }
-                    // Se o mouse estiver mais perto do fundo, rolar para baixo
-                    else if (mouseY > windowHeight - 50) {
-                        if (lastScrollTop !== 1) {
-                            smoothScroll('down');
-                            lastScrollTop = 1;
-                        }
-                    }
-                });
+                setTimeout(function() {
+                    autoScroll = true;
+                    $(".preety-print").show();
+                    $("html, body").animate({ scrollTop: 0 }, 1000);
+                }, 2000);
             });
         });
     });
+    
+    $(window).on('scroll', function() {
+        if (autoScroll) {
+            autoScroll = false;
+        }
+    });
+
+
+    const data = {
+        name: "Vitor Gabriel de Oliveira",
+        profession: "Full Stack Developer",
+        birthYear: 2007,
+        bio: "Since a young age, I've been dedicated to programming and innovation. I am passionate about technology and its power to create solutions and connect people. I enjoy exploring new fields and understanding how my knowledge can interconnect. I am driven by challenges, especially those that seem impossible to solve. I always strive to push my limits and identify areas where I can further develop.",
+        journey: {
+            frontEnd: {
+                start: "HTML, CSS, SCSS, SASS, LESS",
+                frameworks: ["Bootstrap", "UIKit", "Tailwind"],
+                javascript: ["jQuery", "React", "Angular", "Astro", "Vue"]
+            },
+            backEnd: {
+                languages: ["PHP", "Node.js"],
+                tools: ["GraphQL", "Laravel", "REST APIs"]
+            },
+            devOps: {
+                os: "Linux",
+                platforms: ["AWS", "DigitalOcean", "HostGator VPS"],
+                tools: ["Docker", "Kubernetes", "CI/CD", "PM2"]
+            },
+            blockchain: {
+                concepts: ["Blocks", "Binary Trees", "Hash Encryption", "Proof of Work", "Peer-to-Peer Communication"],
+                projects: ["Blockchain Prototype in Node.js", "Optimized Mining with AI"]
+            }
+        },
+        activities: {
+            employment: [
+                {
+                    company: "Clickweb",
+                    location: "Londrina, Paraná, BR",
+                    position: "Full Stack Developer Junior",
+                    startDate: "2024-01-03",
+                    endDate: "Present"
+                },
+                {
+                    company: "Clickweb",
+                    location: "Londrina, Paraná, BR",
+                    position: "Estagiário - Desenvolvedor",
+                    startDate: "2023-10-08",
+                    endDate: "2024-12-31"
+                }
+            ],
+            education: [
+                {
+                    institution: "Marista Escola Social Irmão Acácio",
+                    location: "Londrina, Paraná, BR",
+                    course: "Ensino médio técnico de informática para internet",
+                    startDate: "2022-03-01",
+                    endDate: "2024-12-12"
+                }
+            ]
+        },
+        skills: ["JavaScript", "Node.js", "React", "CSS", "HTML", "Docker"],
+        contact: {
+            email: "vitor@example.com",
+            phone: "+1234567890"
+        }
+    };
+    
+
+    function printJSON() {
+        const prettyJSON = JSON.stringify(data, null, 4);
+        $("#json-output").text(prettyJSON);
+    }
+
+    $(".pretty-render").hide();
+
+    $("#prety").on("change", function() {
+        if ($(this).prop("checked")) {
+            $(".pretty-render").show();
+            $(".pretty-none").hide();
+            printJSON();
+        } else {
+            $(".pretty-none").show();
+            $(".pretty-render").hide();
+        }
+    });
+
 });

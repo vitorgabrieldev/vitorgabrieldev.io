c4b3b20d013c7cb5adc5826f87c05617efeb059d
Vitor Gabriel <vitorgabrieldeoliveiradev@gmail.com>
2026-04-21 17:28:11 -0300

    feat: :sparkles:

--- shared/nav.js
+++ shared/nav.js
@@ -112,7 +112,7 @@
   // ============== SERVICE WORKER ==============
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js').catch(() => {});
+      navigator.serviceWorker.register('sw.js').catch(() => {});
     });
   }
 })();
